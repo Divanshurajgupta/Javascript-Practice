29397354c8a8a82cf550360a519881a1147b5f0b
Divanshu <divanshurahgupta@gmail.com>
2024-05-15 18:16:11 +0530

    Const,let,var

--- 01/Test.js
+++ 01/Test.js
@@ -1,1 +1,7 @@
-console.log("Divanshu")
+const accountID='1233445'
+var accountmail='abc@gmail.com'
+let accountnumber='12334'
+
+accountmail='sdd@gmail.com'
+accountnumber='1233454'
+console.table([accountID,accountmail,accountnumber])
